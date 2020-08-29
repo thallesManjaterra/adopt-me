171c5aaf059c6e8584aeab436adb1fd24946fb12
Thalles Manjaterra <thalles.manjaterra@gmail.com>
2020-08-29 13:39:20 -0300

    Converting to JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
+const Pet = ({ name, animal, breed }) => (
+  <>
+    <h1>{name}</h1>
+    <h2>{animal}</h2>
+    <h2>{breed}</h2>
+  </>
+);
 
 export default Pet;
